3f2461a0046139a386f4edc2de6adc8ab59258f1
jose-rig <jose-rig@student.42malaga.com>
2024-04-16 19:23:08 +0200

    toiii

--- Faltan/ft_atoi.c
+++ Faltan/ft_atoi.c
@@ -6,11 +6,31 @@
 /*   By: jose-rig <jose-rig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 18:52:18 by jose-rig          #+#    #+#             */
-/*   Updated: 2024/04/16 18:53:14 by jose-rig         ###   ########.fr       */
+/*   Updated: 2024/04/16 19:22:56 by jose-rig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    
+	int	i;
+	int	neg;
+	int	res;
+
+	i = 0;
+	neg = 1;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (str[i] - '0') + (res * 10);
+		i++;
+	}
+	return (res * neg);
 }
