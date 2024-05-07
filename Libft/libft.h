da9105c337d75f51cd00a3024316d2ce1cb3d498
jose-rig <jose-rig@student.42malaga.com>
2024-05-07 13:40:28 +0200

    listas

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jose-rig <jose-rig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:29:57 by jose-rig          #+#    #+#             */
-/*   Updated: 2024/05/06 22:33:13 by jose-rig         ###   ########.fr       */
+/*   Updated: 2024/05/07 13:35:39 by jose-rig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,10 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list 	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void(*del)(void*));
+void	ft_lstclear(t_list **lst, void(*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
